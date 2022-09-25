65cde0b7ea780ce585b3caa6ee28d544eef87d48
LiuRion <42686357+1511545190@users.noreply.github.com>
2022-09-25 17:23:48 +0800

    Update 1_3max.c

--- 1_3max.c
+++ 1_3max.c
@@ -9,6 +9,7 @@ int main()
 	int b;
 	int c;
 	//如果使用 , 来分隔输入的 %d, 相应的输入时也需要添加 ,
+	//第一个数据输入后使用了空格 那么第二个数据就不会被记录
 	scanf("%d,%d",&a, &b);
 	printf("a:%d ,b :%d\n");//用后才初始化
 	
@@ -20,8 +21,6 @@ int main()
 }
 int max(int x, int y)
 {
-	if(x > y)
-	return (x);
-	return y;
+	return x > y? x : y;
 	
 }
