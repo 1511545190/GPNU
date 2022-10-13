514a79b5ed05174e5b45555d1ac0610c5b1e62a7
LiuRion <42686357+1511545190@users.noreply.github.com>
2022-10-13 20:34:45 +0800

    Update A~Z大写字母ASCII.c

--- A~Z大写字母ASCII.c
+++ A~Z大写字母ASCII.c
@@ -4,7 +4,7 @@ int main(void)
 {
     for(int i = 32;i<=127; i++)
     {
-        printf("%d represent %c;\n",i,i);  
+        printf("%d represent %c;\n",i,i);   // 48 -> 0        97->a   65 -> A    A - a ->32
         
     }    
     return 0;
