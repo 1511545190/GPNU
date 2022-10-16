28f1912b31363c503229dff9737d1644cfaed390
LiuRion <42686357+1511545190@users.noreply.github.com>
2022-10-16 17:06:19 +0800

    Update do_while循环次数注意.c

--- do_while循环次数注意.c
+++ do_while循环次数注意.c
@@ -1,17 +1,24 @@
-//反复从键盘输入一个整数，如果大于或等于零，累加求和，直到输入负数结束。然后求出平均值并输出。
+
 #include<stdio.h>
 int main()
 {
+  
+ //while !10 = 0 !-10 = 0
+//反复从键盘输入一个整数，如果大于或等于零，累加求和，直到输入负数结束。然后求出平均值并输出。
 int sum = 0; 
 int n ;
 int cn = 0;
 do{
 
 scanf("%d",&n);
-cn ++;
+if(n<0)break;
+cn++;
 sum +=n;
 
-}while(n>=0);   //do出口时，循环的次数是条件执行数+1
-printf("AVE=%.2f\n",1.0*sum/cn-1);
+}while(n>=0);   
+printf("AVE=%.2f\n",1.0*sum/cn);
+  
+  
+  
 return 0;
 }
