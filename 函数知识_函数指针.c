cec63d7106ac17f6bfd4be1ffbd6467aea372fd6
LiuRion <42686357+1511545190@users.noreply.github.com>
2022-10-22 15:40:16 +0800

    Update and rename 函数知识.c to 函数知识_函数指针.c

--- 函数知识_函数指针.c
+++ 函数知识_函数指针.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+int fu(int(*in)())  //以 函数指针 为参数的函数
+{
+    printf("Hello!\n");
+    printf("%d%%",in());  //调用时直接用函数名(), 相当于数组
+    return 0;
+}
+int in(){return 0;}
+
+
 
 void re(int);       //原型声明可以不写参数，但是容易出类型错误
 int main(void)
@@ -19,11 +28,17 @@ int main(void)
         re = 3;
     }
     printf("%d",re);
+    
+    //函数指针
+    int (*fcp)() = fu;  //函数名不加括号就是指针，指向函数所在的地址
+    fcp(in);           //传参直接用函数名
+    
     return 0;
 }
 //函数调用时是传值，里面的操作对外面的空间没有影响
 
 //生存期，作用域  在块中
+
 
 
 
