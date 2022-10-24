9f7630b173e8cfca22ba319b9f89bae2f4d51352
LiuRion <42686357+1511545190@users.noreply.github.com>
2022-10-24 22:01:34 +0800

    Update 程序设计-按要求破解可能硬币.c

--- 程序设计-按要求破解可能硬币.c
+++ 程序设计-按要求破解可能硬币.c
@@ -6,6 +6,15 @@ int main()
     每行输出一种换法，格式为：“fen5:5分硬币数量, fen2:2分硬币数量, 
     fen1:1分硬币数量, total:硬币总数量”。最后一行输出“count = 换法个数”。
     
+输入样例:
+13
+输出样例:
+fen5:2, fen2:1, fen1:1, total:4
+fen5:1, fen2:3, fen1:2, total:6
+fen5:1, fen2:2, fen1:4, total:7
+fen5:1, fen2:1, fen1:6, total:8
+count = 4
+    
     
      */
     int five,two,one;
