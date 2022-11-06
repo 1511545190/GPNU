106516b32ad353ad4f71fdbfeec2472cc8d8b63f
LiuRion <42686357+1511545190@users.noreply.github.com>
2022-11-06 16:51:03 +0800

    Create 内置字符串处理函数.c

---
+++ 内置字符串处理函数.c
@@ -0,0 +1,25 @@
+#include<string.h>
+int main(){
+    char str1[20]={"BBC"};
+    char str2[20]={"Hello"};
+
+    //return the new pointer(pointed of str1) of new string
+    strcat(str1,str2);
+    //You need a str1 to receive the result 
+
+    //return true number of the string(not include the '\0')
+    strlen(str1);
+
+    //return the pointer(pointed of str1) of new string 
+    strcpy(str1,str2);
+    //strcat(str2,"A");
+
+
+
+
+    int a= strcmp(str1,str2);
+    //1 str1 bigger 
+    //0 equal
+    //-1 str2 bigger
+    return 0;
+}
