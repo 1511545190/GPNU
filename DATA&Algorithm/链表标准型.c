802827f68b3a6edb0663d8e4765c8dac76f38fd9
LiuRion <42686357+1511545190@users.noreply.github.com>
2022-11-24 15:20:38 +0800

    Update 链表标准型.c

--- DATA&Algorithm/链表标准型.c
+++ DATA&Algorithm/链表标准型.c
@@ -14,20 +14,29 @@ void create(List *plist,int number)//后插入
 	L node = (L)malloc(sizeof(L));
 	node->value = number;
 	node->next = NULL;
+	
 	L last = plist->head;
-
 	if(!last)// 防止segementationfalut
 	{
 		plist->head = node;
 	}else
 	{
 		for(;last->next;last=last->next);
-
 		node->next = last->next;
 		last->next = node;     //->指向的是真的改变的
 		//后插型，队列
-
 	}
+}
+void crate(L* list,int number)
+{
+    L p = (L)malloc(sizeof(L));
+
+    p->value = number;
+    p->next =NULL;
+    //插到前面去，改头指针,堆栈
+    p->next = *list;
+    *list = p;     //改变指针,用双重指针
+
 }
 
 int main()
