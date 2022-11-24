b21beb1e953a46f4495db2a19dd3cbfd19d939a3
LiuRion <42686357+1511545190@users.noreply.github.com>
2022-11-24 15:18:32 +0800

    Create 链表标准型.c

---
+++ DATA&Algorithm/链表标准型.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node* L;//struct Node 指针
+typedef struct Node{	//main body
+	int value;
+	L next;
+}Node;
+typedef struct Head{
+	L head; 
+}List;			//Head pointer
+
+void create(List *plist,int number)//后插入
+{
+	L node = (L)malloc(sizeof(L));
+	node->value = number;
+	node->next = NULL;
+	L last = plist->head;
+
+	if(!last)// 防止segementationfalut
+	{
+		plist->head = node;
+	}else
+	{
+		for(;last->next;last=last->next);
+
+		node->next = last->next;
+		last->next = node;     //->指向的是真的改变的
+		//后插型，队列
+
+	}
+}
+
+int main()
+{
+	List list;       //set up a head pointer
+	list.head=  NULL;
+	int number;
+	do{
+		scanf("%d", &number);
+		if(number != -1)
+		create(&list,number);
+
+	}while(number != -1);
+
+	printf("\n");
+	L p = list.head;	
+	for( ;p;p=p->next)
+	{
+		printf("%d\n",p->value);
+	}
+	return 0;	
+
+}
